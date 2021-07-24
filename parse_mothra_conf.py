2b1364ab2445ed504bc4399cd7a650f2aefe748b
0x6d61 <0x6d61@gmail.com>
2021-07-24 14:25:59 -0700

    add decrypt_mothra.conf.py

--- parse_mothra_conf.py
+++ parse_mothra_conf.py
@@ -1,3 +1,5 @@
+from struct import unpack
+
 def init_rc4(key):
     index1 = 0
     index2 = 0
@@ -25,5 +27,8 @@ def do_rc4(enc,key):
 key = getBytes(toAddr(0x00416000),0x4)
 enc = bytearray(getBytes(toAddr(0x00416008),0x146))
 decrypted_conf = do_rc4(enc,key)
-print(repr(decrypted_conf))
-print(decrypted_conf)
+print("hostname: {}".format(decrypted_conf[:128]))
+print("port: {}".format(unpack("<h",str(decrypted_conf[128:130]))[0]))
+print("path: {}".format(decrypted_conf[130:258]))
+print("rc4key: {}".format(decrypted_conf[258:322]))
+print("sleep_sec: {}".format(unpack("<i",str(decrypted_conf[322:326]))[0]))
